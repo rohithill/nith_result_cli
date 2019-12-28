733f7c656c0be08ddaf66943dfddb99cc55e8495
Rohit Hill <torohithill@gmail.com>
2019-12-28 16:41:09 +0530

    async implementation completed (basic)

--- download.py
+++ download.py
@@ -1,146 +1,79 @@
-import time
-import concurrent.futures
-import sys
-import json
-import os
-from queue import deque
-import urllib.error
-import socket
-from nith_result import get_result,ROLL_NUMBER_NOT_FOUND
+from utils.departments import departments as dept
+from utils.student import Student
+from nith_result import get_result
 
-# consts contains info related to rollno and branches specific to nith
-# format is starting rollno prefix, total  studetns , width, branch)
-RESULT_DIR = 'result'
+# modified fetch function with semaphore
+import asyncio
+from aiohttp import ClientSession
 
-constants = [('civil','1'),('electrical','2'),('mechanical','3'),('ece','4'),
-            ('cse','5'),('architecture','6'),('chemical','7')]
+# no of results to fetch at the same time
+MAX_CONCURRENT_DOWNLOADS = 1
 
-# Each batch is a 4 tuple entry,
-# prefix,number of students, rollno width, branch name
+async def bound_fetch(sem, url, session,rollno):
+    # Getter function with semaphore.
+    async with sem:
+        await fetch(url, session,rollno)
 
-batches = []
-for branch,c in constants:
-    for y in map(str,range(12,19)): # rollno's from 2012 to 2018
-        if y < '18':
-            batches.append((y+c,99,2,branch))
-        else:
-            # with new batches 3 digits ending is in use. 
-            # Starting from 18 series of roll nos.
-            batches.append((y+c,150,3,branch))
 
-batches.extend([
-    ('15mi4', 70, 2, 'ece_dual'),
-    ('16mi4', 70, 2, 'ece_dual'),
-    ('17mi4', 70 ,2, 'ece_dual'),
-    ('1845', 99 , 2 , 'ece_dual'),
+async def run(roll_number_generator):
+    url = "http://localhost:8080/{}"
+    tasks = []
+    # create instance of Semaphore
+    sem = asyncio.Semaphore(1000)
 
-    ('14mi5', 70, 2, 'cse_dual'),
-    ('15mi5', 70, 2, 'cse_dual'),
-    ('16mi5', 70, 2, 'cse_dual'),
-    ('17mi5', 70, 2, 'cse_dual'),
-    ('1855' , 99, 2, 'cse_dual'),
+    # Create client session that will ensure we dont open new connection
+    # per each request.
+    async with ClientSession() as session:
+        for rollno in roll_number_generator:
+            # pass Semaphore and session to every GET request
+            task = asyncio.ensure_future(bound_fetch(sem, 'http://59.144.74.15/scheme17/studentResult/details.asp', session,rollno))
+            tasks.append(task)
 
-    ('15mi4', 70, 2, 'pg_ece_dual'),
+        responses = asyncio.gather(*tasks)
+        await responses
+    return tasks
+        # print(responses,tasks)
+        # print(tasks[0].result(),dir(tasks[0]))
 
-    ('14mi5', 70, 2, 'pg_cse_dual'),
-    ('15mi5', 70, 2, 'pg_cse_dual'),
-
-    ('178' , 99, 2, 'material'),
-    ('188', 150 , 3, 'material'),
-    ])
-
-# IIIT Una
-batches.extend((f'iiitu{y}1',99,2,'cse_una') for y in ('15','16','17','18'))
-batches.extend((f'iiitu{y}2',99,2,'ece_una') for y in ('15','16','17','18'))
-batches.extend((f'iiitu{y}3',99,2,'it_una') for y in ('17','18'))
-
-# print(batches)
-
-if not os.path.exists(RESULT_DIR):
-    os.mkdir(RESULT_DIR)
-
-def get_batch_result(roll_number_generator,url=None):
+async def get_batch_result(roll_number_generator,url=None):
     batch_result = []
 
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        future_list = deque()
-        for roll_number in roll_number_generator:
-            future = executor.submit(get_result,roll_number,url)
-            future_list.append((future,roll_number))
-        while future_list:
-            future, roll_number = future_list.popleft()
-        # for future, roll_number in future_list:
-            try:
-                data = json.loads(future.result())
-                batch_result.append(data)
-                print('rollnumber: ' , roll_number)
-            except ROLL_NUMBER_NOT_FOUND as e:
-                pass
-            # except urllib.error.URLError as e:
-            except socket.timeout as e:
-            #     # if socket.timeout == e:
-                # print('socket timeout')
-                print(roll_number,e)
-                future_list.append((executor.submit(get_result,roll_number,url),roll_number))
-            except urllib.error.HTTPError as e:
-                print('yaha',roll_number,e)
-            except urllib.error.URLError as e:
-                print('here',roll_number,e)
-                future_list.append((executor.submit(get_result,roll_number,url),roll_number))
-            except Exception as e:
-                print(roll_number,e)
-
-            #     if urllib.error.URLError == e:
-            #         print("Possible timeout error")
-            #     print(f"{roll_number}",e,file=sys.stderr)
-            # else:
-        print('batch_complete')
+  
     return batch_result
 
-def download_result_and_store(roll_number_generator, file_path, url=None):
-    batch_result = get_batch_result(roll_number_generator,url)
-    if (len(batch_result)):
-        with open(file_path,'w') as f:
-            json.dump(batch_result,f)
+async def download_result_and_store(roll_number_generator, file_path, url=None):
+    batch_result = await run(roll_number_generator)
     return len(batch_result)
-
 # sys.exit()
-st = time.time()
 
-for batch in batches:
-    prefix, ending, width, branch = batch
-    print("Processing batch:",prefix,branch)
+async def download_many(session,students):
+    tasks = []
+    for student in students:
+        tasks.append(asyncio.create_task(get_result(session,student)))
+    results = asyncio.gather(*tasks)
+    await results
+    # print(results,tasks,sep='\n')
+    return (task.result() for task in tasks)
+async def download_and_store(session,students,file_name):
+    fp = open(file_name,'w')
+    results = await download_many(session,students)
+    for result in results:
+        fp.write(str(result)+'\n')
 
-    if not os.path.exists(f'{RESULT_DIR}/{branch}'):
-        os.mkdir(f'{RESULT_DIR}/{branch}')
+async def main():
+    myclass = dept.get('CSE_DUAL').get('2017')
+    lt = 100
+    ff = []
+    # for rollno in myclass:
+    #     if not lt:
+    #         break
+    #     lt -= 1
+    #     print(rollno)
+    #     ff.append(Student(rollno))
+    ff.append(Student('17mi562'))
+    async with ClientSession() as session:
+        await download_and_store(session,ff,'./results.txt')
 
-    file_path = f'{RESULT_DIR}/{branch}/batch_{prefix}.json'
-    roll_pattern = prefix + '%s'
-    roll_number_generator = (roll_pattern % str(i).zfill(width) for i in range(1,ending+1))
+if __name__ == '__main__':
+    asyncio.run(main())
 
-    if os.path.isfile(file_path):
-        print(f'File path {file_path} already exists --- Skipping')
-    else:
-        url = None
-        if (branch.startswith('pg')):
-            url = f'http://59.144.74.15/dualdegree{prefix[:2]}/studentResult/details.asp'
-            print(url, ' for pg')
-        no_of_studs = download_result_and_store(roll_number_generator,file_path,url)
-        et = time.time()
-
-        print('Time taken: ',et-st)
-        print(f'Total Students found for {prefix} {branch}:',no_of_studs)
-        print()
-
-    # mtech result of dual degree
-    # if batch[0].startswith('15mi'):
-    #     file_path = f'results/{branch}/batch_{prefix}_mtech.json'
-    #     if os.path.isfile(file_path):
-    #         print(f'File path {file_path} already exists --- Skipping')
-    #     else:
-    #         no_of_studs = download_result_and_store(roll_number_generator,file_path,'http://59.144.74.15/dualdegree15/studentResult/details.asp')
-    #         et = time.time()
-
-    #         print('Time taken: ',et-st)
-    #         print(f'Total Students found for {prefix} {branch}:',no_of_studs)
-    #         print()
--- nith_result.py
+++ nith_result.py
@@ -9,104 +9,10 @@ Try to run it as command line program to see more options.
 '''
 
 import json
-import hashlib
-import urllib.error
-from urllib import parse, request
-from html.parser import HTMLParser
+from utils.parser import ResultParser
+from utils.student import Student, ROLL_NUMBER_NOT_FOUND
 
-class ROLL_NUMBER_NOT_FOUND(Exception): 
-  
-    # Constructor or Initializer 
-    def __init__(self): 
-        self.value = 'Error: Roll No. is invalid.'
-  
-    # __str__ is to print() the value 
-    def __str__(self): 
-        return(self.value) 
-
-class ResultParser(HTMLParser):
-    def custom_init(self):
-        self.inside = False
-        self.tr = False
-        self.td = False
-        self.divt = False
-        self.tables = []
-
-    def handle_starttag(self, tag, attrs):
-        if tag == 'table' and not self.inside:
-            self.tables.append([])
-            self.inside = True
-        if self.inside and tag=='tr':
-            self.tr = True
-            self.tables[-1].append([])
-        if self.tr and tag=='td':
-            self.td = True
-        if not self.inside and tag == 'div':
-            self.divt = True
-
-    def handle_data(self,data):
-        if self.td:
-            data = data.strip()
-            if data:
-                self.tables[-1][-1].append(data)
-        if self.divt and not self.inside:
-            data = data.strip()
-            if data:
-                data = [i.strip() for i in data.split(':')]
-                if data[0] == 'Semester':
-                    self.tables.append([data[1]])
-
-    def handle_endtag(self, tag):
-        if tag=='td':
-            self.td = False
-        elif tag=='tr':
-            self.tr = False
-        elif tag == 'table':
-            self.inside = False
-        if tag == 'div' and self.divt:
-            self.divt = False
-
-class Student:
-    def __init__(self, roll_number,url=None):
-        self.roll_number = roll_number
-        self.url = url or self.get_result_url()
-        # print(self.url)
-    def get_result_student(self):
-        data = parse.urlencode({'RollNumber':self.roll_number}).encode()
-
-        # print(self.url)
-        req = request.Request(self.url,data)
-        the_page = b''
-        error = False
-        # try:
-        with request.urlopen(req,timeout=15) as response:
-            the_page = response.read()
-        # print('---------------------\n',the_page)
-        # except urllib.error.HTTPError as e:
-            # error = e
-        # if error:
-        #     import sys
-        #     print(error,file=sys.stderr)
-        if 'redirect_dict' in dir(req):
-            # print("redirected")
-            raise ROLL_NUMBER_NOT_FOUND
-        # return page content with error (which is roll no. not found). Magic number is hash of error page
-        return the_page
-
-    def get_result_url(self):
-        url = "http://59.144.74.15/{}{}/studentResult/details.asp"
-        if self.roll_number.startswith('iiitu'):
-            year = self.roll_number[5:7]
-            college_code = 'IIITUNA'
-        else:
-            year = self.roll_number[:2]
-            college_code = 'scheme'
-        return url.format(college_code,year)
-
-    def __str__(self):  
-        return f"Roll_number: {self.roll_number}"
-
-def get_result(roll_number: str,url=None):
+async def get_result(session,student):
     '''
     Accepts a roll_number and optionally a url to fetch result from.
     May raise an exception.
@@ -119,27 +25,35 @@ def get_result(roll_number: str,url=None):
     [<Table of result for <Semester>],
     [<Summary of result for <Semester>]]
     '''
-    stud = Student(roll_number,url)
-    result = stud.get_result_student()
-    result = result.decode()
-    # print(result)
-    r = ResultParser()
-    r.custom_init()
-    r.feed(result)
-    r.tables[0][0][1] = r.tables[0][0][1].replace('\xa0','')
-    return json.dumps(r.tables)
+    async with session.post(student.url,data=student.data) as response:
+        result = await response.text()
+        # print(result)
+        parser = ResultParser()
+        parser.custom_init()
+        try:
+            parser.feed(result)
+            parser.tables[0][0][1] = parser.tables[0][0][1].replace('\xa0','')
+        except Exception as e:
+            print(e)
+            # return
+        # Remove '\xa0' from the name of the student
+        return parser.tables
 
-def main():
+async def main():
     import sys
     import argparse
+    import aiohttp
     parser = argparse.ArgumentParser()
     parser.add_argument("roll_number",help="download this roll_number's result")
     parser.add_argument("--html",action="store_true",help="Generates html output of the result")
     parser.add_argument("--url",help="specify url for the result")
     args = parser.parse_args()
     try:
-        result = get_result(args.roll_number,args.url)
-        result = json.loads(result)
+        async with aiohttp.ClientSession() as session:
+            student = Student(args.roll_number,args.url)
+            # print(student)
+            result = await get_result(session,student)
+        # print(result)
         if args.html:
             table_number = 0
             name_table = result[0]
@@ -157,10 +71,12 @@ def main():
                 print('</table>')
                 table_number += 1
         else:
-            print(result)
+            for r in result:
+                print(r,sep='\n')
     except Exception as e:
         print('--->',e)
         # sys.exit(0)
 
 if __name__ == '__main__':
-    main()
+    import asyncio
+    asyncio.run(main())
